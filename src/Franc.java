51adb1d0181b4f1860c895ba462fc1dee4f07beb
gkdev <gkolotov.adv@gmail.com>
2023-11-14 23:38:46 +0200

    feat: implement franc

---
+++ src/Franc.java
@@ -0,0 +1,17 @@
+public class Franc {
+    private int amount;
+
+    Franc(int amount) {
+        this.amount = amount;
+    }
+
+    Franc times(int multiplier) {
+        return new Franc(amount * multiplier);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Franc dollar = (Franc) obj;
+        return amount == dollar.amount;
+    }
+}
