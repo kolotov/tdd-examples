21ea165fe4dd8198d96a9125242ea1263eeea9b9
gkdev <gkolotov.adv@gmail.com>
2023-11-14 21:41:40 +0200

    feat: implement simple class

--- test/DollarTest.java
+++ test/DollarTest.java
@@ -5,7 +5,7 @@ public class DollarTest {
     @Test
     public void testMultiplication() {
         Dollar five = new Dollar(5);
-        five.times(5);
+        five.times(2);
         assertEquals(10, five.amount);
     }
 }
