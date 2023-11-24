1a1670c1c0c597df8f02faf7006ecb997353a36a
gkdev <gkolotov.adv@gmail.com>
2023-11-25 00:41:39 +0200

    feat: create specific currency via fabric methods

--- src/Money.java
+++ src/Money.java
@@ -1,9 +1,19 @@
-public class Money {
+public abstract class Money {
     protected int amount;
 
     public boolean equals(Object obj) {
         Money money = (Money) obj;
         return amount == money.amount
-        && getClass().equals(obj.getClass());
+                && getClass().equals(obj.getClass());
     }
+
+    public static Money dollar(int amount) {
+        return new Dollar(amount);
+    }
+
+    public static Money franc(int amount) {
+        return new Franc(amount);
+    }
+
+    public abstract Money times(int multiplier);
 }
