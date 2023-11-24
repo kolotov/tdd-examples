e3f3dbe854ff248fe4c95fd6557928c04a88884b
gkdev <gkolotov.adv@gmail.com>
2023-11-25 00:20:08 +0200

    feat: multi currency compare

--- src/Money.java
+++ src/Money.java
@@ -3,6 +3,7 @@ public class Money {
 
     public boolean equals(Object obj) {
         Money money = (Money) obj;
-        return amount == money.amount;
+        return amount == money.amount
+        && getClass().equals(obj.getClass());
     }
 }
